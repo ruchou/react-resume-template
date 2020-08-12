084e7911b35faf92f7bfd995156a6807faad44e7
ruchou <joe04024@gapp.nthu.edu.tw>
2020-08-12 16:21:10 +0800

    refactor Resume.js

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -8,28 +8,32 @@ class Resume extends Component {
     render() {
         if (this.props.data) {
             var education = this.props.data.education.map(function (education) {
-                return <div key={education.school}><h3>{education.school}</h3>
-                    <p className="info">{education.degree} <span>&bull;</span><em
-                        className="date">{education.graduated}</em> <br/>
-                        {education.ranking}
-                    </p>
-                    <p>{education.description}</p></div>
+                return( <div key={`edcucation_${education.school}`}>
+                            <h3>{education.school}</h3>
+                                <p className="info">{education.degree} <span>&bull;</span><em
+                                    className="date">{education.graduated}</em> <br/>
+                                    {education.ranking}
+                                </p>
+                                <p>{education.description}</p>
+                        </div>
+                )
             });
 
-            var work = this.props.data.work.map(function (work) {
-                const listWorkDescription = work.description.map((item, i) =>
-                    <li key={i}>{item}</li>
+            var work = this.props.data.work.map( (work) => {
+                const listWorkDescription = work.description.map((w, idx) =>
+                    <li key={`work_${idx}`}>{w}</li>
                 );
 
-                return <div key={work.company}><h3>{work.company}</h3>
-                    <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-                    <p>{listWorkDescription}</p>
-                </div>
+                return( <div key={work.company}>
+                            <h3>{work.company}</h3>
+                            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+                            <p>{listWorkDescription}</p>
+                        </div>)
             });
-            var skills = this.props.data.skills.map(function (skills) {
+            var skills = this.props.data.skills.map( (skills)=> {
                 var className = 'bar-expand ' + skills.name.toLowerCase();
-                return <li key={skills.name}><span style={{width: skills.level}}
-                                                   className={className}> </span><em>{skills.name}</em></li>
+                return( <li key={`skill_${skills.name}`}><span style={{width: skills.level}}
+                                                   className={className}> </span><em>{skills.name}</em></li>)
             })
         }
 
